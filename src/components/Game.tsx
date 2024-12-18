d660423f93ef19ab687999e791c3ace991f9a114
Eugene Mishura <eugene.mishura@oxheadalpha.com>
2024-12-18 14:12:02 -0500

    game score component

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,17 +1,70 @@
 import { GameBoard } from "./Board";
 import { GameCtx } from "../App";
 import assertNever from "../assert-never";
+import { Board } from "../domain/board";
 
 export function Game({ game, onAction }: GameCtx) {
   switch (game.kind) {
     case "play":
-      return <GameBoard board={game.board} onAction={onAction} />;
+      return <GamePlay game={game} onAction={onAction} />;
     case "won":
-      return <GameBoard board={game.board} onAction={onAction} />;
+      return <GameWon game={game} onAction={onAction} />;
     case "lost":
-      return <GameBoard board={game.board} onAction={onAction} />;
+      return <GameLost game={game} onAction={onAction} />;
     default:
       assertNever(game);
       return <></>;
   }
+}
+
+function GameScore({ board, message }: { board: Board; message: string }) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        margin: "1rem",
+        fontWeight: "bold",
+        fontSize: "2em",
+      }}
+    >
+      <div>Mines: {board.minesCount}</div>
+      <div
+        style={{
+          color: "blue",
+        }}
+      >
+        {message}
+      </div>
+      <div>Uncovered: {board.uncoveredCount}</div>
+    </div>
+  );
+}
+
+function GamePlay({ game, onAction }: GameCtx) {
+  return (
+    <div>
+      <GameScore board={game.board} message="***" />
+      <GameBoard board={game.board} onAction={onAction} />;
+    </div>
+  );
+}
+
+function GameWon({ game, onAction }: GameCtx) {
+  return (
+    <div>
+      <GameScore board={game.board} message="You won!!!" />
+      <GameBoard board={game.board} onAction={onAction} />
+    </div>
+  );
+}
+
+function GameLost({ game, onAction }: GameCtx) {
+  return (
+    <div>
+      <GameScore board={game.board} message="You lost" />
+      <GameBoard board={game.board} onAction={onAction} />
+    </div>
+  );
 }
