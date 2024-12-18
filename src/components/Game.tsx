b31b56e5567e0b233a1e0d5429fabfb708684338
Eugene Mishura <eugene.mishura@oxheadalpha.com>
2024-12-18 15:58:35 -0500

    UI for end of game

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -2,13 +2,28 @@ import { GameBoard } from "./Board";
 import { GameCtx } from "../App";
 import { Board } from "../domain/board";
 import { match } from "ts-pattern";
+import { ReactNode } from "react";
 
-export const Game = ({ game, onAction }: GameCtx) =>
-  match(game)
-    .with({ kind: "play" }, () => <GamePlay game={game} onAction={onAction} />)
-    .with({ kind: "won" }, () => <GameWon game={game} onAction={onAction} />)
-    .with({ kind: "lost" }, () => <GameLost game={game} onAction={onAction} />)
-    .exhaustive();
+export const Game = ({ game, onAction }: GameCtx) => (
+  <div
+    style={{
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      rowGap: "1rem",
+    }}
+  >
+    {match(game)
+      .with({ kind: "play" }, () => (
+        <GamePlay game={game} onAction={onAction} />
+      ))
+      .with({ kind: "won" }, () => <GameWon game={game} onAction={onAction} />)
+      .with({ kind: "lost" }, () => (
+        <GameLost game={game} onAction={onAction} />
+      ))
+      .exhaustive()}
+  </div>
+);
 
 function GameScore({ board, message }: { board: Board; message: string }) {
   return (
@@ -17,7 +32,6 @@ function GameScore({ board, message }: { board: Board; message: string }) {
         display: "flex",
         justifyContent: "space-between",
         alignItems: "center",
-        margin: "1rem",
         fontWeight: "bold",
         fontSize: "2em",
       }}
@@ -37,27 +51,58 @@ function GameScore({ board, message }: { board: Board; message: string }) {
 
 function GamePlay({ game, onAction }: GameCtx) {
   return (
-    <div>
+    <>
       <GameScore board={game.board} message="***" />
       <GameBoard board={game.board} onAction={onAction} />;
-    </div>
+    </>
   );
 }
 
 function GameWon({ game, onAction }: GameCtx) {
   return (
-    <div>
+    <>
       <GameScore board={game.board} message="You won!!!" />
       <GameBoard board={game.board} onAction={onAction} />
-    </div>
+      <div>
+        <ActionButton onClick={() => onAction({ kind: "reset" })}>
+          ResetGame
+        </ActionButton>
+      </div>
+    </>
   );
 }
 
 function GameLost({ game, onAction }: GameCtx) {
   return (
-    <div>
+    <>
       <GameScore board={game.board} message="You lost" />
       <GameBoard board={game.board} onAction={onAction} />
-    </div>
+      <div>
+        <ActionButton onClick={() => onAction({ kind: "reset" })}>
+          Reset Game
+        </ActionButton>
+      </div>
+    </>
+  );
+}
+
+function ActionButton({
+  onClick,
+  children,
+}: {
+  onClick: () => void;
+  children: ReactNode;
+}) {
+  return (
+    <button
+      style={{
+        fontWeight: "bold",
+        fontSize: "2em",
+        backgroundColor: "#8080FF",
+      }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
   );
 }
